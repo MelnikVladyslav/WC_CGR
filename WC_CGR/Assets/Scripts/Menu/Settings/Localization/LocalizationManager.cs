fcb3fab76894d76bc0b379ae244d9bc2a909b811
Vlad <115177420+MelnikVladyslav@users.noreply.github.com>
2024-01-28 13:59:29 +0200

    Add menu

---
+++ WC_CGR/Assets/Scripts/Menu/Settings/Localization/LocalizationManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Menu.Settings.Localization
+{
+    public class LocalizationManager : MonoBehaviour
+    {
+        public void Ukr()
+        {
+            string language = "Ukr";
+            PlayerPrefs.SetString("Language", language);
+        }
+
+        public void Eng()
+        {
+            string language = "Eng";
+            PlayerPrefs.SetString("Language", language);
+        }
+    }
+}
---
+++ WC_CGR/Assets/Scripts/Menu/Settings/Localization/TextLanguage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Menu.Settings.Localization
+{
+    public class TextLanguage : MonoBehaviour
+    {
+        public string language;
+        Text text;
+
+        public string textUkr;
+        public string textEng;
+
+        void Start()
+        {
+            text = GetComponent<Text>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            language = PlayerPrefs.GetString("Language");
+
+            if (language == "" || language == "Eng")
+            {
+                text.text = textEng;
+            }
+            else if (language == "Ukr")
+            {
+                text.text = textUkr;
+            }
+        }
+    }
+}
